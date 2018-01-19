5e055478436a1774ee9bb88d16652b5705716704
xinxin <xinxin_949421@126.com>
2018-01-19 11:24:45 +0800

    by: xinxin mybatis组合使用

---
+++ springboot/src/main/java/com/sauzny/springboot/mybatis/package-info.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+
+/***************************************************************************
+* @文件名称: package-info.java
+*
+* @包路径  : com.sauzny.springboot.mybatis 
+*				 
+* @版权所有: Personal xinxin (C) 2017
+*
+* @类描述:   TODO
+* 
+* @创建人:   ljx 
+*
+* @创建时间: 2018年1月19日 - 上午11:22:31 
+*	
+***************************************************************************/
+
+package com.sauzny.springboot.mybatis;
+
+/*
+        <!-- Spring Boot Mybatis 依赖 -->
+        <dependency>
+            <groupId>org.mybatis.spring.boot</groupId>
+            <artifactId>mybatis-spring-boot-starter</artifactId>
+            <version>1.3.1</version>
+        </dependency>
+    
+                        目录结构，自动代码生成插件等功能，参考jkitchen_mvc项目
+    
+*/
